a5614bed5c1d7ecc1e09013dad04fd83e386894b
Pedro Henrique <31496260+pewhenrique@users.noreply.github.com>
2017-10-15 18:03:32 -0300

    Add files via upload

---
+++ C/1005.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//This program is goint to calculate the average between A and B.
+int main(void){
+    double A,B,MEDIA;
+    scanf("%lf", &A);
+    scanf("%lf", &B);
+    A*=3.5;
+    B*=7.5;
+    MEDIA = ((A+B)/(3.5+7.5));
+    printf("MEDIA = %.5lf\n", MEDIA);
+return 0;
+}
