77fe4af16321a90793b09cfb0cd8815994ae5f82
Pedro Henrique <31496260+pewhenrique@users.noreply.github.com>
2017-10-15 16:44:50 -0300

    Add files via upload

---
+++ C/1003.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+//This program is going to sum the value of the variables A and B.
+int main(void){
+    int A,B,SOMA;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    SOMA=A+B;
+    printf("SOMA = %d\n", SOMA);
+return 0;
+}
