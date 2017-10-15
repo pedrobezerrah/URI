df2b4319d09ac64f9b9b6879c3b53954f9c64098
Pedro Henrique <31496260+pewhenrique@users.noreply.github.com>
2017-10-15 17:25:08 -0300

    Add files via upload

---
+++ C/1004.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+//This program is going to multiply the value of the variables A and B.
+int main(void){
+    int A,B,PROD;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    PROD=A*B;
+    printf("PROD = %d\n", PROD);
+return 0;
+}
